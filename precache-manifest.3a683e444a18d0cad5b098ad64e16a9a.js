fd7a1d7d2bc398577c0ddcf6429375a601c6f4d2
kirubhasangar <charmingfox007@gmail.com>
2020-04-02 16:01:33 +0530

    Updates

---
+++ precache-manifest.3a683e444a18d0cad5b098ad64e16a9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3fb8e107195c24b822044ca006c872",
+    "url": "/COVID-19-Tamilnadu/index.html"
+  },
+  {
+    "revision": "6259fe228d561ca28043",
+    "url": "/COVID-19-Tamilnadu/static/css/main.22ec6237.chunk.css"
+  },
+  {
+    "revision": "a537ebfd9145c3aa9cd7",
+    "url": "/COVID-19-Tamilnadu/static/js/2.7ab81be1.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/COVID-19-Tamilnadu/static/js/2.7ab81be1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6259fe228d561ca28043",
+    "url": "/COVID-19-Tamilnadu/static/js/main.5e63ff93.chunk.js"
+  },
+  {
+    "revision": "b153f164191d47537f19",
+    "url": "/COVID-19-Tamilnadu/static/js/runtime-main.7753cbc0.js"
+  }
+]);
